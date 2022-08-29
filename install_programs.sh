5792a88b1f41b645a717133b70361a10b8842731
alouest <julien.marzin.44@gmail.com>
2022-08-29 10:08:00 +0200

    add python3.8-venv for ubuntu

--- install_programs.sh
+++ install_programs.sh
@@ -4,7 +4,7 @@
 red='\033[0;31m'
 blue='\033[0;34m'
 NC='\033[0m' # No Color
-programms="zsh terminator vim tree git curl gparted python3-pip" 
+programms="zsh terminator vim tree git curl gparted python3-pip python3.8-venv" 
 
 cd ~
 echo -e ${blue}'apt update & upgrade'${NC}
